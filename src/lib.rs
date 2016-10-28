bb49e39424e295a4f961cdf99433e5225ed54e69
Bob Somers <bob@bobsomers.com>
2016-10-27 23:08:26 -0700

    Basic structs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+struct Topic {
+    name: String,
+}
+
+struct Publisher {
+
+}
+
+struct DataWriter {
+
+}
+
+struct Subcriber {
+
+}
+
+struct DataReader {
+
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
